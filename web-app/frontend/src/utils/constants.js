36115c62bb64d0ba8a1bfaf9065fb51e0a89ea2a
prayushaaaa <prayusha000@gmail.com>
2024-12-05 09:06:47 +0545

    back to development

--- web-app/frontend/src/utils/constants.js
+++ web-app/frontend/src/utils/constants.js
@@ -1,8 +1,8 @@
 import UserData from "../views/plugin/UserData";
 
-export const API_BASE_URL = "https://django-test.maya-wears.com/api/v1/";
-export const SERVER_URL = "https://django-test.maya-wears.com";
-export const CLIENT_URL = "https://test.maya-wears.com/";
+export const API_BASE_URL = "http://127.0.0.1:8000/ ";
+export const SERVER_URL = "http://127.0.0.1:8000";
+export const CLIENT_URL = "http://localhost:5173/";
 export const PAYPAL_CLIENT_ID = "test";
 export const CURRENCY_SIGN = "$";
 export const userId = UserData()?.user_id;
